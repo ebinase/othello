0ad34d45a339b6c1f155ada94e78de14e4a64814
ebina <ebina.engr@gmail.com>
2022-03-17 03:15:31 +0900

    ルーティング系 #32

--- routes/web.php
+++ routes/web.php
@@ -17,33 +17,31 @@ Route::get('/', function () {
     return view('top.index');
 })->name('top');
 
-Route::prefix('game')->group(function() {
-    Route::name('game.')->group(function() {
-        // ゲームメニューを表示
-        Route::get('/', [
-            'as' => 'index',
-            'uses' => 'App\Http\Controllers\GameController@index'
-        ]);
-        // ゲームを初期化
-        Route::get('/start/{game_mode}', [
-            'as' => 'start',
-            'uses' => 'App\Http\Controllers\GameController@start'
-        ]);
-        // ゲーム画面を表示
-        Route::get('/show/{game_id}', [
-            'as' => 'show',
-            'uses' => 'App\Http\Controllers\GameController@show'
-        ]);
-        // ゲームの結果
-        Route::get('/result/{game_id}', [
-            'as' => 'showResult',
-            'uses' => 'App\Http\Controllers\GameController@showResult'
-        ]);
+Route::controller(App\Http\Controllers\GameController::class)->prefix('game')->name('game.')->group(function() {
+    // ゲームメニューを表示
+    Route::get('/', [
+        'as' => 'index',
+        'uses' =>'index'
+    ]);
+    // ゲームを初期化
+    Route::get('/start/{game_mode}', [
+        'as' => 'start',
+        'uses' => 'start'
+    ]);
+    // ゲーム画面を表示
+    Route::get('/show/{game_id}', [
+        'as' => 'show',
+        'uses' => 'show'
+    ]);
+    // ゲームの結果
+    Route::get('/result/{game_id}', [
+        'as' => 'showResult',
+        'uses' => 'showResult'
+    ]);
 
-        // ターンを更新する
-        Route::post('/process', [
-            'as' => 'process',
-            'uses' => 'App\Http\Controllers\GameController@process'
-        ]);
-    });
+    // ターンを更新する
+    Route::post('/process', [
+        'as' => 'process',
+        'uses' => 'App\Http\Controllers\GameController@process'
+    ]);
 });
